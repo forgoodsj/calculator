e3c41bd4604402a4063c852afff229b00f670c7e
forgoodsj <877848146@qq.com>
2017-06-20 00:36:40 +0800

    解二元一次方程

--- .idea/index.py
+++ .idea/index.py
@@ -1,1 +1,8 @@
+#coding:utf-8
 __author__ = 'jun'
+from sympy import *
+x = Symbol('x')
+y = Symbol('y')
+
+#print solve(x * 2 - 4 ,x) #第一个参数为要解的方程，方程要求右边等于0第二参数是要解的未知数
+print solve([2*x-y-3,3*x+y-7],[x,y])
